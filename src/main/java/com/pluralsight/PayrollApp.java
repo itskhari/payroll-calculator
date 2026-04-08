a2ee3502a542bb53781dd4b50e998f0af13edfc3
itkhari <mrkharij1@gmail.com>
2026-04-08 15:48:39 -0400

    made payroll calc

--- src/main/java/com/pluralsight/PayrollApp.java
+++ src/main/java/com/pluralsight/PayrollApp.java
@@ -1,7 +1,25 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("What's your name?: ");
+        String name = sc.nextLine();
+
+        System.out.print("Okay " + name + ", how many hours did you work this pay period?: ");
+        double hoursWorked = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("Great! Now what is your hourly wage?: ");
+        double hourlyWage = sc.nextDouble();
+        sc.nextLine();
+
+        double pay = hoursWorked * hourlyWage;
+
+        System.out.println("Calculating your pay.");
+        System.out.printf("Your pay is $%.2f for this period", pay);
     }
 }
